e2807d4a6e6c54e5d1f3cc17365f10ad3b1974be
Patricio Rosso <patriciosimonrosso@gmail.com>
2023-04-08 19:09:12 -0300

    setup

---
+++ Models/Vuelo.cs
@@ -0,0 +1,18 @@
+﻿namespace VuelosCRUD.Models
+{
+    public class Vuelo
+    {
+        public int Id { get; set; }
+
+        public string FlightNumber { get; set; } = null!;
+
+        public string Airline { get; set; } = null!;
+
+        public bool Delayed { get; set; } = false;
+
+
+
+
+
+    }
+}
