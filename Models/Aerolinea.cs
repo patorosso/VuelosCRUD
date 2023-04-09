151f4144c56dab55108afe337736a3b670d2298b
Patricio Rosso <patriciosimonrosso@gmail.com>
2023-04-09 12:37:30 -0300

    finish model vuelo -aerolinea. add migration

--- Models/Aerolinea.cs
+++ Models/Aerolinea.cs
@@ -1,17 +1,24 @@
-﻿namespace VuelosCRUD.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VuelosCRUD.Models
 {
     public class Aerolinea
     {
         public int Id { get; set; }
 
+        [StringLength(maximumLength: 100)]
         public string? Name { get; set; }
 
+        [StringLength(maximumLength: 15)]
         public string? Iata { get; set; }
 
+        [StringLength(maximumLength: 15)]
         public string? Icao { get; set; }
 
+        [StringLength(maximumLength: 35)]
         public string? CallSign { get; set; }
 
+        [StringLength(maximumLength: 100)]
         public string? Country { get; set; }
 
         public bool Active { get; set; }
--- Models/Vuelo.cs
+++ Models/Vuelo.cs
@@ -1,13 +1,20 @@
-﻿namespace VuelosCRUD.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VuelosCRUD.Models
 {
     public class Vuelo
     {
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(maximumLength: 12)]
         public string FlightNumber { get; set; } = null!;
 
+        [Required]
+        [StringLength(maximumLength: 70)]
         public string Airline { get; set; } = null!;
 
+        [Required]
         public bool Delayed { get; set; } = false;
 
 
