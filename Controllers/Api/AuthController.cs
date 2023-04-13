0890b421fbaf8492c4d6190c1161f05c8a8b3c11
Patricio Rosso <patriciosimonrosso@gmail.com>
2023-04-13 17:11:20 -0300

    change images path. change identity migration name to avoid errors and add api controller auth.

---
+++ Controllers/Api/AuthController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using VuelosCRUD.Areas.Identity.Data;
+using VuelosCRUD.Models;
+
+namespace VuelosCRUD.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<VuelosCRUDUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AuthController(RoleManager<IdentityRole> roleManager, UserManager<VuelosCRUDUser> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> SeedRoles()
+        {
+            bool userExists = await _roleManager.RoleExistsAsync(StaticUserRoles.User);
+            bool adminExists = await _roleManager.RoleExistsAsync(StaticUserRoles.Admin);
+
+            if (userExists && adminExists)
+            {
+                return Ok("Los roles ya estaban cargados.");
+            }
+
+            await _roleManager.CreateAsync(new IdentityRole(StaticUserRoles.Admin));
+            await _roleManager.CreateAsync(new IdentityRole(StaticUserRoles.User));
+
+            return Ok("Los roles fueron correctamente cargados.");
+        }
+    }
+}
--- Migrations/20230411173825_identity.cs
+++ Migrations/20230411173825_identity.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore.Metadata;
+﻿using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
@@ -7,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace VuelosCRUD.Migrations
 {
     /// <inheritdoc />
-    public partial class identity : Migration
+    public partial class Identity : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
